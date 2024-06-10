0d7ee218c2ca588b9ced987582ac0a0b562d716a
Mariano Altieri <marianoa@givingassistant.org>
2024-06-10 12:21:44 -0300

    Improved products grid responsiveness

--- src/app/(products)/components/ProductCard.tsx
+++ src/app/(products)/components/ProductCard.tsx
@@ -16,7 +16,7 @@ export const ProductCard = async (props: Props) => {
   const { product, imagePriority = false } = props;
 
   return (
-    <Card className="w-full md:max-w-[300px] p-3">
+    <Card className="w-full p-3">
       <Link href={`/product/${product.id}`}>
         <Image
           src={product.thumbnail}
--- src/app/(products)/components/ProductsGrid.tsx
+++ src/app/(products)/components/ProductsGrid.tsx
@@ -9,7 +9,7 @@ export const ProductsGrid = (props: Props) => {
   const { products } = props;
 
   return (
-    <div className="flex gap-3 justify-start flex-wrap">
+    <div className="grid grid-cols-products gap-3">
       {products.map((product, index) => (
         <ProductCard key={product.id} product={product} imagePriority={index < 6} />
       ))}
